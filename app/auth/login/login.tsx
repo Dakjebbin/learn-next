3aaab431c2270574995d5c7864ef4a79e2e25efa
DakJebbin <Jebbinp@gmail.com>
2025-02-11 00:38:31 +0100

    dashboard working in now

--- app/auth/login/login.tsx
+++ app/auth/login/login.tsx
@@ -1,53 +1,76 @@
+import { signIn } from "@/auth";
 
-
-import { signIn } from '@/auth'
-
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='flex flex-col text-white justify-center items-center '>
-        <div className='bg-[#000000b3] sm:p-16 pt-16'>
-                    <p className='text-3xl mb-10 font-bold'>Sign In</p>
+    <div className="flex flex-col text-white justify-center items-center ">
+      <div className="bg-[#000000b3] sm:p-16 pt-16">
+        <p className="text-3xl mb-10 font-bold">Sign In</p>
         <form action="">
-            <div className='w-[300px] mb-7'>
-            <input className='bg-transparent rounded-md border py-3 px-4 w-full border-white outline-red-700 text-white' required placeholder='Email' type="email" name="" id="1" />
-            </div>
-            
-            <div className='w-[300px] mb-4'>
-            <input className='bg-transparent rounded-md border py-3 px-4 w-full border-white outline-red-700 text-white' required placeholder='Password' type="password" name="" id="2" />
-            </div>
-            <button className='w-full bg-[#c11119] py-2 rounded-md font-semibold hover:bg-[]'>Sign In</button>
+          <div className="w-[300px] mb-7">
+            <input
+              className="bg-transparent rounded-md border py-3 px-4 w-full border-white outline-red-700 text-white"
+              required
+              placeholder="Email"
+              type="email"
+              name=""
+              id="1"
+            />
+          </div>
+
+          <div className="w-[300px] mb-4">
+            <input
+              className="bg-transparent rounded-md border py-3 px-4 w-full border-white outline-red-700 text-white"
+              required
+              placeholder="Password"
+              type="password"
+              name=""
+              id="2"
+            />
+          </div>
+          <button className="w-full bg-[#c11119] py-2 rounded-md font-semibold hover:bg-[]">
+            Sign In
+          </button>
         </form>
 
-        <p className='text-center text-xl my-3'>OR</p>
+        <p className="text-center text-xl my-3">OR</p>
 
-        <form action= {
-          async () => {
-            'use server'
-            await signIn('google',  { redirectTo: "/dashboard" })
-          }}>
-        <button type='submit' className='w-full bg-[#464343a6] py-2 rounded-md font-semibold hover:bg-[]'>Sign In Using Google</button>
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google", { redirectTo: "/dashboard" });
+          }}
+        >
+          <button
+            type="submit"
+            className="w-full bg-[#464343a6] py-2 rounded-md font-semibold hover:bg-[]"
+          >
+            Sign In Using Google
+          </button>
         </form>
 
-        
+        <p className="my-5 text-center">Forgot Password?</p>
 
-        <p className='my-5 text-center'>
-            Forgot Password?
+        <div className="flex my-3">
+          <input className="mr-2" type="checkbox" name="" />
+          <p>Remember Me</p>
+        </div>
+
+        <p>
+          New to Netflix?{" "}
+          <Link
+            className="cursor-pointer hover:underline font-semibold"
+            href="/"
+          >
+            {" "}
+            Sign Up Now.{" "}
+          </Link>
         </p>
-
-        <div className='flex my-3'>
-            <input className='mr-2' type="checkbox" name="" />
-            <p>Remember Me</p>
-        </div>
-
-        <p>New to Netflix? <Link className='cursor-pointer hover:underline font-semibold' href= "/"> Sign Up Now. </Link></p>
-        </div>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,27 +2,38 @@
 import React from 'react'
 import { auth, signOut } from '@/auth'
 import Image from 'next/image'
+import { assets } from '@/public/asset'
 
 const Page = async () => {
     const session = await auth()
-       
+       if (!session) return <div className='text-white'>Not Authenticated Please Login</div>
   return (
+     
     <div className='text-white'>
-        <h1>Welcome {session?.user?.name}</h1>
+<div className='flex items-center mx-10 justify-between'>
+      <div className='w-56'>
+      <Image className='w-full' src={assets.logo} alt='Logo'/>
+      </div>
+      <div className='flex items-center'>
+        <div className='flex items-center mr-10'>
+        <h1 className='text-xl mr-4 font-bold'>Welcome {session?.user?.name}</h1>
        
        {session?.user?.image &&
-        <Image src={session?.user?.image} width={100} height={100} alt="user Image"  />
+        <Image className='w-8 rounded-full' src={session?.user?.image} width={100} height={100} alt="user Image"  />
        }
+       </div>
         <form action={async () => {
             'use server'
 
             await signOut( {redirectTo: '/'})
             
         }}>
-            <button type='submit'>Logout</button>
+            <button className=' bg-red-700 px-3 py-1 rounded-md font-semibold' type='submit'>Logout</button>
         </form>
-       
+        </div>
+        </div>
     </div>
+      
   )
 }
 
